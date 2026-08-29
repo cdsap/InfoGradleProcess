718d7e418a7be6e3232998ef911b4da6af83fc5e
Iñaki Villar <inaki.seri@gmail.com>
2026-08-29 02:36:12 +0200

    fix: refactor process collection to shared core helper (#54)

--- src/test/kotlin/io/github/cdsap/gradleprocess/GradleProcessCollectorTest.kt
+++ src/test/kotlin/io/github/cdsap/gradleprocess/GradleProcessCollectorTest.kt
@@ -1,7 +1,6 @@
 package io.github.cdsap.gradleprocess
 
 import io.github.cdsap.jdk.tools.parser.model.TypeProcess
-import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -9,21 +8,16 @@ import org.junit.Test
 class GradleProcessCollectorTest {
 
     @Test
-    fun collectConsolidatesProvidersWithRequestedTypeProcess() {
-        val project = ProjectBuilder.builder().build()
-        val jStat = project.provider {
-            """
+    fun collectConsolidatesStringsWithRequestedTypeProcess() {
+        val jStat = """
             Timestamp         S0C         S1C         S0U         S1U          EC          EU          OC          OU          MC          MU        CCSC       CCSU     YGC     YGCT     FGC    FGCT     CGC    CGCT       GCT
                     1117.8          0.0      30720.0          0.0      30720.0    1135616.0     755712.0     865280.0     546816.0     195184.0    189433.3      22208.0    20357.8     22    0.682     0    0.000      12    0.070     0.752
             28743
             """.trimIndent()
-        }
-        val jInfo = project.provider {
-            """
+        val jInfo = """
             -XX:+UseParallelGC -XX:MaxHeapSize=536870912
             28743
             """.trimIndent()
-        }
 
         val processes = GradleProcessCollector().collect(jStat, jInfo, TypeProcess.Gradle)
 
@@ -35,20 +29,15 @@ class GradleProcessCollectorTest {
 
     @Test
     fun collectPreservesKotlinTypeProcessForDevelocityPath() {
-        val project = ProjectBuilder.builder().build()
-        val jStat = project.provider {
-            """
+        val jStat = """
             Timestamp         S0C         S1C         S0U         S1U          EC          EU          OC          OU          MC          MU        CCSC       CCSU     YGC     YGCT     FGC    FGCT     CGC    CGCT       GCT
                     1117.8          0.0      30720.0          0.0      30720.0    1135616.0     755712.0     865280.0     546816.0     195184.0    189433.3      22208.0    20357.8     22    0.682     0    0.000      12    0.070     0.752
             28743
             """.trimIndent()
-        }
-        val jInfo = project.provider {
-            """
+        val jInfo = """
             -XX:+UseParallelGC -XX:MaxHeapSize=536870912
             28743
             """.trimIndent()
-        }
 
         val processes = GradleProcessCollector().collect(jStat, jInfo, TypeProcess.Kotlin)
 
@@ -57,12 +46,8 @@ class GradleProcessCollectorTest {
     }
 
     @Test
-    fun collectReturnsEmptyListWhenProvidersHaveNoMatchingProcesses() {
-        val project = ProjectBuilder.builder().build()
-        val jStat = project.provider { "xxxx" }
-        val jInfo = project.provider { "yyyy" }
-
-        val processes = GradleProcessCollector().collect(jStat, jInfo, TypeProcess.Gradle)
+    fun collectReturnsEmptyListWhenStringsHaveNoMatchingProcesses() {
+        val processes = GradleProcessCollector().collect("xxxx", "yyyy", TypeProcess.Gradle)
 
         assertTrue(processes.isEmpty())
     }
